b1bf61b29b95e4a03d85e777da7b8507048e1a10
Robert Balatbat <73982521+robjgb@users.noreply.github.com>
2024-12-28 03:57:51 -0800

    adding content

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./_includes/**/*.html",             
+    "./_layouts/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
